f96a436e1c867fdc1679b63232db632f762d8700
Kamil Zaleski <kamil.zaleski@gmail.com>
2012-11-22 22:28:31 +0100

    move terminal to own module

--- app2.js
+++ app2.js
@@ -3,11 +3,10 @@ var express = require('express')
   , http = require('http')
   , util = require('util')
   , events = require('events')
-  , exec = require('child_process').exec
   , _ = require('underscore')
   , colors = require('colors')
-  , utils = require('./lib/utils')
-  , pty = require('pty.js');
+  , term = require('./lib/terminal')
+  , utils = require('./lib/utils');
 
 
 var app = express();
@@ -58,135 +57,6 @@ c = {
 }
 
 
-var Terminal = function(conf) {
-  events.EventEmitter.call(this);
-  var self = this;
-
-  var max = {
-    inactivity: 0.5,
-    cpu: 90,
-    ram: 10
-  };
-
-  var time_start = new Date().getTime(),
-      time_last = time_start;
-
-  var last_status = {},
-      dout = 0,
-      din = 0,
-      alive = false,
-      monitor_cycle,
-      history_cycles = 5, // seconds
-      history = { cpu: [], ram: [], };
-
-  var proc = pty.spawn(conf.exec, conf.args, {
-    name: 'xterm',
-    cols: 80,
-    cwd: process.env.HOME,
-    env: process.env
-  });
-
-  proc.on('exit',  function() {
-    console.log('TERM -> EXIT');
-    alive = false;
-  });
-  proc.on('error', function() { console.log('TERM -> ERROR'); });
-  proc.on('close', function() {
-    console.log('TERM -> CLOSE');
-    alive = false;
-  });
-
-  proc.on('data', function(data) {
-    if (!alive) {
-      alive = true;
-    }
-    self.emit('data',data);
-    dout += _.size(data);
-    self.update();
-  });
-
-  this.write = function(data) {
-    if (alive) {
-      din += _.size(data);
-      proc.write(data);
-      self.update();
-    }
-  }
-
-  this.getLastStatus = function() {
-    return last_status;
-  }
-
-  this.update = function() {
-    time_last = new Date().getTime();
-  }
-
-  this.status = function() {
-    _.each(history, function (value,key) {
-      
-    });
-  }
-
-  this.updateStatus = function(stats) {
-    exec('ps -o pcpu,rss,pid -p '+proc.pid,function(a,out,c) {
-      try {
-        out = out.match(/[0-9]*\.?[0-9]/g);
-        last_status = {
-          cpu: parseFloat(out[0]),
-          ram: parseInt(out[1]),
-          pid: parseInt(out[2]),
-          dataOut: dout,
-          dataIn: din,
-          inactive: parseInt((new Date().getTime()-time_last) / 1000),
-          live: parseInt((new Date().getTime()-time_start) / 1000)
-        };
-
-        _.each(["cpu","ram"], function(key){
-          if ( history[key].push(last_status[key]) > history_cycles ) {
-            history[key].shift();
-          }
-          last_status[key+"_avg"] = _.reduce(history[key], function(m, n){ return m+n; }) / history[key].length;
-        });
-
-        stats(last_status);
-      } catch(err) {
-        alive = false;
-        stats(false)
-      }
-    });
-  }
-
-  this.kill = function(reason) {
-    if (alive) {
-      alive = false;
-      self.emit('msg',"KILL: "+reason);
-      exec('kill '+proc.pid);
-      setTimeout(function() {
-        self.updateStatus(function(status){
-          if (status) {
-            exec('kill -9 '+status["pid"]);
-          } else {
-            self.emit('killed');
-            clearInterval(monitor_cycle);
-          }
-        });
-      },5000); // try hard killing after 5 sec
-    }
-  }
-
-  monitor_cycle = setInterval(function() {
-    if (alive) {
-      self.updateStatus(function(status) {
-        if (status["cpu_avg"]   > max.cpu ) { self.kill('avg cpu over '+max.cpu+'%');      }
-        if (status["ram_avg"]   > max.ram*1024 ) { self.kill('avg ram over '+max.ram+'MB');     }
-        if (status["inactive"]  > max.inactivity*60 ) { self.kill('inactivity over '+max.inactivity+' min');  }
-      });
-    }
-  },1000);
-
-}
-util.inherits(Terminal, events.EventEmitter);
-
 var TerminalManager = function(conf) {
   events.EventEmitter.call(this);
   var self = this, connections = {}, entries = {};
@@ -204,7 +74,7 @@ var TerminalManager = function(conf) {
 
   this.request = function(socket,entry) {
 
-    var terminal = new Terminal({
+    var terminal = new term.Terminal({
       exec : 'mongo',
       args : [entry.mongo["name"]]
     });
@@ -253,7 +123,7 @@ var TerminalManager = function(conf) {
   setInterval(self.send_update,1000)
 
 }
-util.inherits(Terminal, events.EventEmitter);
+util.inherits(TerminalManager, events.EventEmitter);
 
 
 
